c222b3736d42e7fe88307e95b1ffbb3d59a2af44
Cuong <dcongcuong2012@gmail.com>
2026-01-17 20:22:18 +0700

    add apigatway

--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -2,7 +2,7 @@
 
 public record OrderName
 {
-    private const int DefaultLength = 4;
+    private const int DefaultLength = 5;
     public string Value { get;} = default!;
 
     private OrderName(string value) => Value = value;
